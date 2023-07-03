636f2c3e3afd1603c9dce0a89b30d8d8815a85cb
Konstantin Lisitsky <kapurelloruru@gmail.com>
2023-07-03 11:51:29 +0200

    Finished task 8, array methods practice

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,17 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: Array<UserType>, action: ActionType): Array<UserType> => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            return [...state].sort((user1, user2) => {
+                return action.payload === 'up'
+                    ? user1.name.localeCompare(user2.name)
+                    : user2.name.localeCompare(user1.name)
+            }) // need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            return [...state].filter(u => u.age >= 18) // need to fix
         }
         default:
             return state

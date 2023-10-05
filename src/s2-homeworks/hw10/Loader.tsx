45d5a60c866571a546ca1bc887559170930ee494
Konstantin <kapurelloruru@gmail.com>
2023-10-05 16:03:56 +0200

    fix bugs

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,5 +1,5 @@
 import s from './Loader.module.css'
-import loader from './Double Ring-2.9s-197px.svg'
+import loader from './DoubleRing.svg'
 export const Loader = () => <div className={s.loader}>
   <img src={loader} alt="loader"/>
 </div>

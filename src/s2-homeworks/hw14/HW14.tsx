90c185e38b97fa42c19fbe096ce5bb4d1243d72f
Konstantin Lisitsky <kapurelloruru@gmail.com>
2024-01-21 14:05:23 +0100

    Finished task 14

--- src/s2-homeworks/hw14/HW14.tsx
+++ src/s2-homeworks/hw14/HW14.tsx
@@ -16,7 +16,7 @@ import {useSearchParams} from 'react-router-dom'
 const getTechs = (find: string) => {
     return axios
         .get<{ techs: string[] }>(
-            'https://incubator-personal-page-back.herokuapp.com/api/3.0/homework/test2',
+            'https://samurai.it-incubator.io/api/3.0/homework/test2',
             {params: {find}}
         )
         .catch((e) => {
@@ -32,22 +32,22 @@ const HW14 = () => {
 
     const sendQuery = (value: string) => {
         setLoading(true)
-        getTechs(value)
-            .then((res) => {
+        getTechs(value).then((res) => {
                 // делает студент
-
+                // @ts-ignore
+            setTechs(res.data.techs)
+            setLoading(false)
                 // сохранить пришедшие данные
-
                 //
             })
+
     }
 
     const onChangeText = (value: string) => {
         setFind(value)
         // делает студент
-
         // добавить/заменить значение в квери урла
-        // setSearchParams(
+        setSearchParams({find: value})
 
         //
     }
--- src/s2-homeworks/hw14/common/c8-SuperDebouncedInput/SuperDebouncedInput.tsx
+++ src/s2-homeworks/hw14/common/c8-SuperDebouncedInput/SuperDebouncedInput.tsx
@@ -33,7 +33,10 @@ const SuperDebouncedInput: React.FC<SuperDebouncedInputPropsType> = (
 
         if (onDebouncedChange) {
             // делает студент
-
+            clearInterval(timerId)
+            setTimerId(+setTimeout(() => {
+                onDebouncedChange(value)
+            }, 1500))
             // остановить предыдущий таймер
             // запустить новый на 1500ms, в котором вызовется функция
 
